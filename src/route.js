e50e4e1118ca39b8e1a1884dbf8730c831a796d7
davidtheblane <davidtheblane@gmail.com>
2021-06-25 00:54:34 -0300

    all pages included in src/views

--- src/route.js
+++ src/route.js
@@ -4,8 +4,10 @@ const express = require("express");
 // variavel route contem todas as routes do express
 const route = express.Router();
 
-// passando a rota da pagina inicial
+// passando a rota das paginas
 route.get("/", (req, res) => res.render("index"));
+route.get("/room", (req, res) => res.render("room"));
+route.get("/create-pass", (req, res) => res.render("create-pass"));
 
 // exportando a constante route para que o server.js possa enxergá-la
 module.exports = route;
