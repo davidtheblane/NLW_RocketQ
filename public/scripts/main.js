c10d9bded1f16c8e506e011e4cc9ad54e0542530
davidtheblane <davidtheblane@gmail.com>
2021-06-25 12:18:56 -0300

    all buttons and actions in room page are ok

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,23 +4,40 @@ import Modal from "./modal.js";
 // adicionando ela a uma constante
 const modal = Modal();
 
-// Quando o botao marcar como lida for clicado ele abre a modal
+const modalTitle = document.querySelector(".modal h2");
+const modalDescription = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
+
+// Ações do botão de Marcar como lida
 //pegar todos os botoes que existem com a class check
 const checkButtons = document.querySelectorAll(".actions a.check");
 
 checkButtons.forEach((button) => {
-  // adicionar a escuta para abrir o modal ao clicar no botão.
-  button.addEventListener("click", (event) => {
-    //Abrir modal
-    modal.open();
-  });
+  // adicionar a escuta
+  button.addEventListener("click", handleClick);
 });
 
-// Quando o botao delete for clicado ele abre a modal
+// Ações do botão de excluir
 const deleteButton = document.querySelectorAll(".actions a.delete");
 
 deleteButton.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    modal.open();
-  });
+  button.addEventListener("click", (event) => handleClick(event, false));
 });
+
+// Abrir modal e modificar os textos
+function handleClick(event, check = true) {
+  // previne que ao clicar nos botoes eles alterem a url
+  event.preventDefault();
+
+  const text = check ? "Marcar como lida" : "Excluir";
+
+  modalTitle.innerHTML = `${text}`;
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`;
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`;
+
+  // muda a cor do botao dependendo da ação escolhida
+  check
+    ? modalButton.classList.remove("red")
+    : modalButton.classList.add("red");
+  modal.open();
+}
