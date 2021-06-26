aab264f683bc13dd33f114c68907dcbfa9510828
davidtheblane <davidtheblane@gmail.com>
2021-06-26 13:30:59 -0300

    prevent room number from duplicity

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,20 +6,33 @@ module.exports = {
     const pass = req.body.password;
 
     let roomId;
+    let isRoom = true;
 
-    for (let i = 0; i < 6; i++) {
-      i == 0
-        ? (roomId = Math.floor(Math.random() * 10).toString())
-        : (roomId += Math.floor(Math.random() * 10).toString());
+    while (isRoom) {
+      // GERA O NUMERO DA SALA
+      for (let i = 0; i < 6; i++) {
+        i == 0
+          ? (roomId = Math.floor(Math.random() * 10).toString())
+          : (roomId += Math.floor(Math.random() * 10).toString());
+      }
+
+      // VERIFICA SE O NUMERO JA EXISTE
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+
+      isRoom = roomsExistIds.some((roomExistId) => roomExistId === roomId);
+
+      // Se não existir
+      if (!roomsExistIds) {
+        // INSERE A SALA NO BANCO
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES(
+            ${parseInt(roomId)},
+            ${pass}
+        )`);
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES(
-        ${parseInt(roomId)},
-        ${pass}
-    )`);
 
     db.close();
 
