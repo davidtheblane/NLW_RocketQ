0a747ba6ad24ed958868c6c6ca8d69c9ddcf323a
davidtheblane <davidtheblane@gmail.com>
2021-06-26 00:16:25 -0300

    init.js done

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,16 @@
+const Database = require("../db/config");
+
 module.exports = {
-  create(req, res) {
-    let roomId = 123456;
+  async create(req, res) {
+    const db = await Database();
+
+    let roomId;
+
+    for (let i = 0; i < 6; i++) {
+      i == 0
+        ? (roomId = Math.floor(Math.random() * 10).toString())
+        : (roomId += Math.floor(Math.random() * 10).toString());
+    }
 
     res.redirect(`/room/${roomId}`);
   },
