969d3d15f97f837759e268e0042318879d450f37
davidtheblane <davidtheblane@gmail.com>
2021-06-25 23:14:50 -0300

    subs all script/, images/, styles/ for /script/,/images/, /styles/ in ejs files

--- src/route.js
+++ src/route.js
@@ -1,6 +1,7 @@
 // importando
 const express = require("express");
 const QuestionController = require("./controllers/QuestionController");
+const RoomController = require("./controllers/RoomController");
 
 // variavel route contem todas as routes do express
 const route = express.Router();
@@ -11,9 +12,11 @@ route.get("/create-pass", (req, res) =>
   res.render("index", { page: "create-pass" })
 );
 
-route.get("/room", (req, res) => res.render("room"));
+route.get("/room/:room", (req, res) => res.render("room"));
+
 // formato que o formulario de dentro da modal tem que passar a informação
 route.post("/room/:room/:question/:action", QuestionController.index);
+route.post("/room/create-room", RoomController.create);
 
 // exportando a constante route para que o server.js possa enxergá-la
 module.exports = route;
