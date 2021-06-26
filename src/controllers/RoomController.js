12d3b78654160d70279296fdb96d86f8e40752e9
davidtheblane <davidtheblane@gmail.com>
2021-06-26 18:30:36 -0300

    no-question message done

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -47,11 +47,19 @@ module.exports = {
     const questionsRead = await db.all(
       `SELECT * FROM questions WHERE room = ${roomId} and read = 1`
     );
+    let isNoQuestions;
+
+    if (questions.length == 0) {
+      if (questionsRead.length == 0) {
+        isNoQuestions = true;
+      }
+    }
 
     res.render("room", {
       roomId: roomId,
       questions: questions,
       questionsRead: questionsRead,
+      isNoQuestions: isNoQuestions,
     });
   },
 
