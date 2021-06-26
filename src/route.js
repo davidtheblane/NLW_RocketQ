7f129be07101b033658090d58710b1699c117c59
davidtheblane <davidtheblane@gmail.com>
2021-06-25 21:42:46 -0300

    new folder parts with create-pass and enter-room

--- src/route.js
+++ src/route.js
@@ -6,10 +6,12 @@ const QuestionController = require("./controllers/QuestionController");
 const route = express.Router();
 
 // passando a rota das paginas
-route.get("/", (req, res) => res.render("index"));
-route.get("/room", (req, res) => res.render("room"));
-route.get("/create-pass", (req, res) => res.render("create-pass"));
+route.get("/", (req, res) => res.render("index", { page: "enter-room" }));
+route.get("/create-pass", (req, res) =>
+  res.render("index", { page: "create-pass" })
+);
 
+route.get("/room", (req, res) => res.render("room"));
 // formato que o formulario de dentro da modal tem que passar a informação
 route.post("/room/:room/:question/:action", QuestionController.index);
 
