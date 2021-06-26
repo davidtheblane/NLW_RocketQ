fd8e044a51fdaaaae3517dc3cb3aefabdb4fe52c
davidtheblane <davidtheblane@gmail.com>
2021-06-26 00:26:43 -0300

    ready for insert data in database

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -3,6 +3,7 @@ const Database = require("../db/config");
 module.exports = {
   async create(req, res) {
     const db = await Database();
+    const pass = req.body.password;
 
     let roomId;
 
@@ -11,6 +12,16 @@ module.exports = {
         ? (roomId = Math.floor(Math.random() * 10).toString())
         : (roomId += Math.floor(Math.random() * 10).toString());
     }
+
+    await db.run(`INSERT INTO rooms (
+      id,
+      pass
+    ) VALUES(
+        ${parseInt(roomId)},
+        ${pass}
+    )`);
+
+    db.close();
 
     res.redirect(`/room/${roomId}`);
   },
